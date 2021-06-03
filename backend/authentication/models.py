3cb20aa2e795f09e3164aa2086a7ff4cfc3e614e
Ismaquias Farias <ismaquias@gmail.com>
2021-06-02 22:59:33 -0300

    criaçao dos models

--- backend/authentication/models.py
+++ backend/authentication/models.py
@@ -1,6 +1,8 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+	email = models.EmailField (unique=True)
+	
+	def __str__(self):
+		return
--- backend/forum/models.py
+++ backend/forum/models.py
@@ -1,6 +1,30 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Post(models.Model):
+	user = models.FogeignKey (settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+	canal = models.FogeignKey ("Canal", on_delete=models.CASCADE)
+	data_cracao = models.DateTimeField (auto_now_add=True)
+	texto = models.TextField (max_length=255)
+	titulo = models.CharField (max_length=255)
+	likes = models.BigIntegerField (default=0)
+	deslikes = models.BigIntegerField (default=0)
+	
+	def __str__(self):
+		return f"Post <titulo: {self.titulo}>"
+		
+class Comentario (models.Model):
+	user = models.FogeignKey (settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+	post = models.FogeignKey ("Post", on_delete=models.CASCADE)
+	data_cracao = models.DateTimeField (auto_now_add=True)
+	texto = models.TextField (max_length=255)
+
+	def __str__(self):
+		return f"Comentario <{self.id}>"
+
+class Canal (models.Model):
+	user = models.FogeignKey (settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+	tema = models.CharField(max_length=255)
+	
+	def __str__(self):
+		return f"Canal <{self.id}>"
