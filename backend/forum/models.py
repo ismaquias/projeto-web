c6a6e2e574a88619f853ba4f1a174ec725766fd0
Ismaquias Farias <ismaquias@gmail.com>
2021-06-02 23:57:20 -0300

    models

--- backend/forum/models.py
+++ backend/forum/models.py
@@ -2,8 +2,8 @@ from django.db import models
 from django.conf import settings
 
 class Post(models.Model):
-	user = models.FogeignKey (settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-	canal = models.FogeignKey ("Canal", on_delete=models.CASCADE)
+	user = models.ForeignKey (settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+	canal = models.ForeignKey ("Canal", on_delete=models.CASCADE)
 	data_cracao = models.DateTimeField (auto_now_add=True)
 	texto = models.TextField (max_length=255)
 	titulo = models.CharField (max_length=255)
@@ -14,8 +14,8 @@ class Post(models.Model):
 		return f"Post <titulo: {self.titulo}>"
 		
 class Comentario (models.Model):
-	user = models.FogeignKey (settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-	post = models.FogeignKey ("Post", on_delete=models.CASCADE)
+	user = models.ForeignKey (settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+	post = models.ForeignKey ("Post", on_delete=models.CASCADE)
 	data_cracao = models.DateTimeField (auto_now_add=True)
 	texto = models.TextField (max_length=255)
 
@@ -23,7 +23,7 @@ class Comentario (models.Model):
 		return f"Comentario <{self.id}>"
 
 class Canal (models.Model):
-	user = models.FogeignKey (settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+	user = models.ForeignKey (settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 	tema = models.CharField(max_length=255)
 	
 	def __str__(self):
